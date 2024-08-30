d325680b386a3d4eb697dc26d16603955b8a6b2b
Samruddh pk <107286956+Samruddhpk@users.noreply.github.com>
2024-08-30 12:26:52 +0530

    register & login page structure

--- 5_Redux+Query+Router+Axios/1_Decora/src/pages/Login.jsx
+++ 5_Redux+Query+Router+Axios/1_Decora/src/pages/Login.jsx
@@ -1,6 +1,29 @@
+import { Form, Link } from "react-router-dom";
+
+import { FormInput, SubmitBtn } from "../components";
+
 const Login = () => {
     return (
-        <div>Login</div>
+        <section className="h-screen grid place-items-center">
+            <Form method="post" className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
+                <h4 className="text-center text-3xl font-bold">Login</h4>
+                <FormInput type="email" label="email" name="identifier" defaultValue="test@test.com" />
+                <FormInput type="password" label="password" name="password" defaultValue="secret" />
+
+                <div className="mt-4">
+                    <SubmitBtn text="login" />
+                </div>
+                <button type="button" className="btn btn-secondary btn-block capitalize">
+                    guest user
+                </button>
+                <p className="text-center">Not a member yet?
+                    <Link to="/register" className="ml-2 link link-hover link-primary capitalize">
+                        register
+                    </Link>
+                </p>
+
+            </Form>
+        </section>
     );
 };
 export default Login;
--- 5_Redux+Query+Router+Axios/1_Decora/src/pages/Register.jsx
+++ 5_Redux+Query+Router+Axios/1_Decora/src/pages/Register.jsx
@@ -1,6 +1,29 @@
+import { FormInput, SubmitBtn } from "../components";
+import { Form, Link } from "react-router-dom";
+
 const Register = () => {
+
     return (
-        <div>Register</div>
+        <section className="h-screen grid place-items-center">
+            <Form method="post" className="w-96 card p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
+                <h4 className="font-bold text-3xl text-center">
+                    Register
+                </h4>
+                <FormInput type="text" label="username" name="username" />
+                <FormInput type="email" label="email" name="email" />
+                <FormInput type="password" label="password" name="password" />
+
+                <div className="mt-4">
+                    <SubmitBtn text="register" />
+                </div>
+
+                <p className="text-center">Already a member?
+
+                    <Link to="/login" className="link link-hover link-primary capitalize ml-2">login</Link>
+                </p>
+
+            </Form>
+        </section>
     );
 };
 export default Register;
