c43a2b8308cdba2216c38f87b1cb445df83fb8bb
Samruddh pk <107286956+Samruddhpk@users.noreply.github.com>
2024-08-30 18:20:45 +0530

    added pagination

--- 5_Redux+Query+Router+Axios/1_Decora/src/components/PaginationContainer.jsx
+++ 5_Redux+Query+Router+Axios/1_Decora/src/components/PaginationContainer.jsx
@@ -1,6 +1,60 @@
+import { useNavigate, useLoaderData, useLocation } from "react-router-dom";
+
 const PaginationContainer = () => {
+    const { meta } = useLoaderData();
+    const { pageCount, page } = meta.pagination;
+    const pages = Array.from({ length: pageCount }, (_, index) => {
+        return index + 1;
+    });
+
+    const { search, pathname } = useLocation();
+    const navigate = useNavigate();
+
+    const handlePageChange = (pageNumber) => {
+        const searchParams = new URLSearchParams(search);
+        searchParams.set("page", pageNumber);
+        navigate(`${pathname}?${searchParams.toString()}`);
+    };
+
+    if (pageCount < 2) return null;
+
     return (
-        <div>PaginationContainer</div>
+        <div className="mt-16 flex justify-end">
+            <div className="join">
+                <button
+                    className="btn btn-xs sm:btn-md join-item"
+                    onClick={() => {
+                        let prevPage = page - 1;
+                        if (prevPage < 1) prevPage = pageCount;
+                        handlePageChange(prevPage);
+                    }}
+                >
+                    Prev
+                </button>
+                {pages.map((pageNumber) => {
+                    return (
+                        <button
+                            onClick={() => handlePageChange(pageNumber)}
+                            key={pageNumber}
+                            className={`btn btn-xs sm:btn-md border-none join-item ${pageNumber === page ? "bg-base-300 border-base-300" : ""
+                                }`}
+                        >
+                            {pageNumber}
+                        </button>
+                    );
+                })}
+                <button
+                    className="btn btn-xs sm:btn-md join-item"
+                    onClick={() => {
+                        let nextPage = page + 1;
+                        if (nextPage > pageCount) nextPage = 1;
+                        handlePageChange(nextPage);
+                    }}
+                >
+                    Next
+                </button>
+            </div>
+        </div>
     );
 };
 export default PaginationContainer;
